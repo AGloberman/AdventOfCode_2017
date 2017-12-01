402dda6a90ab962523dc3a2d934385729fc42925
Aaron Globerman <agloberman@ea.com>
2017-12-01 09:47:46 -0800

    Optimize

--- day1/day1.go
+++ day1/day1.go
@@ -3,16 +3,13 @@ package day1
 func Captcha (input string) int {
 	var sum, nextIndex int
 	for index := 0; index < len(input); index++  {
-		char := input[index] - 48
 		nextIndex = index + 1
 		if int32(index) == int32(len(input) - 1) {
 			nextIndex = 0
 		}
 
-		nextChar := input[nextIndex] - 48
-
-		if int(char) == int(nextChar) {
-			sum += int(char)
+		if input[index] == input[nextIndex] {
+			sum += int(input[index]) - 48
 		}
 	}
 
@@ -23,16 +20,14 @@ func Captcha2 (input string) int {
 	var sum, nextIndex int
 	indexIncrement := int(len(input)) / 2
 	for index := 0; index < len(input); index++  {
-		char := input[index] - 48
 		nextIndex = index + indexIncrement
 		if nextIndex >= len(input) {
 			nextIndex = nextIndex - len(input)
 		}
 
-		nextChar := input[nextIndex] - 48
+		if input[index] == input[nextIndex] {
+			sum += int(input[index]) - 48
 
-		if int(char) == int(nextChar) {
-			sum += int(char)
 		}
 	}
 
